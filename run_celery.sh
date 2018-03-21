db96afcdce56b50f064f8ad45e95a36801dd671b
Dspatharakis <dhmhtrhs.spath@gmail.com>
2018-03-21 12:55:22 +0200

    fix code

--- run_celery.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# wait for RabbitMQ server to start
-sleep 10
-
-  
-# run Celery worker for our project myproject with Celery configuration stored in Celeryconf
-"celery -A ca_tf worker --app=ca_tf.celery_app:app --loglevel=info"
-
