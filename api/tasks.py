c9fc1d8886f0a904d594784a01647d12d9b4c667
Dspatharakis <dhmhtrhs.spath@gmail.com>
2018-05-25 10:55:53 -0400

    asychronous requests server side

--- api/tasks.py
+++ api/tasks.py
@@ -3,41 +3,22 @@ from __future__ import absolute_import, unicode_literals
 from celery import task
 import os.path
 import logging
+from api.models import Tasks_Interval
+from django.db import transaction, OperationalError
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
 
 @task()
-def aggregate_stats():
-    print('Aggregating Interval Stats')
-    cwd = os.path.dirname(__file__)
-
-    # Calculate Interval's Submitted Requests
-    with open(os.path.join(cwd, '../logs/submitted.log'), 'a+') as f:
-        sub_size = sum(1 for _ in f)
-    os.remove(os.path.join(cwd, '../logs/submitted.log'))
-
-    # Calculate Interval's Finished Requests
-    with open(os.path.join(cwd, '../logs/finished.log'), 'a+') as f:
-        fin_size = sum(1 for _ in f)
-    os.remove(os.path.join(cwd, '../logs/finished.log'))
-
-    # Calculate Interval's Average Response Time
-    with open(os.path.join(cwd, '../logs/rt.log'), 'a+') as f:
-        rts = [float(a.strip()) for a in f]
-    os.remove(os.path.join(cwd, '../logs/rt.log'))
-
-    if len(rts) == 0:
-        interval_art = "NaN"
-    else:
-        interval_art = sum(rts)/len(rts)
-
-    # print sub_size
-    # print fin_size
-    logger.info('Average Interval Response Time: ' + str(interval_art))
-
-    with open(os.path.join(cwd,'../logs/intervals.csv'), 'a') as f:
-        f.write(str(str(sub_size) + ',' + str(fin_size) + ',' + str(interval_art) + '\n'))
-
-
-
+#@transaction.atomic
+def post_submitted(request,filename):
+    while True:       # it should work withour while loop but it seems that atomic transaction misses some submitted requests. 
+        with transaction.atomic():
+            s = Tasks_Interval.objects.select_for_update().first()
+            submitted = s.submitted
+            s.submitted = submitted + 1
+            try:
+                s.save()
+                break
+            except OperationalError:
+                print("DB locked: concurrency avoided")
